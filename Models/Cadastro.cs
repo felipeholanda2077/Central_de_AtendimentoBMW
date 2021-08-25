0c2987a35f0436d8660c18144864f2af3eba284b
felipeholanda2077 <felipe_holandafreitas@hotmail.com>
2021-08-25 15:31:33 -0300

    ATV-3

---
+++ Models/Cadastro.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+namespace ATV_3_UC5.Models
+{
+    public class Cadastro
+    {
+        public string descrição{get; set;}
+
+        public double valor_unitario{get; set;}
+
+        public int quantidade{get; set;}
+
+    }
+}
